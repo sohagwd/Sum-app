e040abd65c1d521f5c2fcee28e79dfd18548d09c
Jubayed-A <learnigprograming@gmail.com>
2023-12-08 03:37:02 +0600

    this app has been improve

--- app/src/main/java/com/example/sumoftwonumber/MainActivity.kt
+++ app/src/main/java/com/example/sumoftwonumber/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -13,17 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val num1 = findViewById(R.id.editText1) as EditText
         val num2 = findViewById(R.id.editText2) as EditText
-        val tot = findViewById(R.id.text) as TextView
-        val btn = findViewById(R.id.button1) as Button
+        val total = findViewById(R.id.tvDisplay) as TextView
+        val btnSum = findViewById(R.id.button1) as Button
 
-        btn.setOnClickListener {
-            val val1 = num1.text.toString().toInt()
-            val val2 = num2.text.toString().toInt()
+        btnSum.setOnClickListener {
+            val firstNum = num1.text.toString().toInt()
+            val secondNum = num2.text.toString().toInt()
 
-            val  result = val1 + val2;
-            tot.setText(result.toString())
+            if (firstNum !== null && secondNum !== null) {
+                val result = firstNum + secondNum;
+                total.text = result.toString()
+            } else {
+                Toast.makeText(this, "Input all the fill", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 
